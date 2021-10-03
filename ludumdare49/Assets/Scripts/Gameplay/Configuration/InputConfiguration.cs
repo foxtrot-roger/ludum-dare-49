4048980b08de597c99f62f6facb3d2567dfb3ba2
foxtrot-roger <36508665+foxtrot-roger@users.noreply.github.com>
2021-10-03 17:39:31 +0200

    Added images and fixes to levels

--- ludumdare49/Assets/Scripts/Gameplay/Configuration/InputConfiguration.cs
+++ ludumdare49/Assets/Scripts/Gameplay/Configuration/InputConfiguration.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Gameplay/Input")]
 public class InputConfiguration : ScriptableObject
 {
+    public string HorizontalAxisName = "Horizontal";
+    public string JumpAxisName = "Jump";
     public KeyCode Left = KeyCode.Q;
     public KeyCode Right = KeyCode.D;
     public KeyCode Jump = KeyCode.Space;
 
     public float ReadHorizontal()
-        => (Input.GetKey(Left) ? -1 : 0) + (Input.GetKey(Right) ? 1 : 0);
+        //=> (Input.GetKey(Left) ? -1 : 0) + (Input.GetKey(Right) ? 1 : 0);
+        => Input.GetAxis(HorizontalAxisName);
     public bool ReadJump()
-        => Input.GetKeyDown(Jump);
+        //=> Input.GetKeyDown(Jump);
+        => Input.GetButtonDown(JumpAxisName);
 }
